f0ab424af1820ee113a470929feeaa52b4bc0187
Santiago <santiagoquinteroperfil@gmail.com>
2023-03-21 16:32:26 -0400

    Corrigiendo lógica de título para que se agrege directamente con boton

--- javascript/creador.js
+++ javascript/creador.js
@@ -6,7 +6,7 @@ const divCreador = document.querySelector(".creador");
 const form = document.querySelector(".form");
 buttonCreator.addEventListener("click", creador);
 
-function selecctionInput() {
+function selectionInput() {
 	const li = document.querySelectorAll("li");
 	li.forEach((a) => {
 		a.querySelectorAll("input").forEach((b) => {
@@ -36,10 +36,23 @@ function creador(event) {
 		containerQuestions.classList.add("main__container__trivia");
 		container.appendChild(containerQuestions);
 		const h2 = document.createElement("h2");
-		const nameInput = (h2.innerText = prompt("Inserta la pregunta"));
-		const correctAnswer = prompt("Inserta  el número de la respuesta correcta");
+        const inputPregunta=document.createElement("input");
+        inputPregunta.type="text";
+        containerQuestions.appendChild(inputPregunta) ;
+		const nameInput = ( inputPregunta.value);
 		h2.classList.add("main__list__tittle");
 		containerQuestions.appendChild(h2);
+        const buttonPregunta=document.createElement("button");
+        buttonPregunta.innerText="Crear";
+        buttonPregunta.classList.add("buttonPregunta");
+        containerQuestions.appendChild(buttonPregunta);
+
+        buttonPregunta.addEventListener("click",()=>{
+            h2.innerText=inputPregunta.value;
+            buttonPregunta.classList.add("remover")
+            inputPregunta.classList.add("remover")
+            console.log("hola")
+        })
 		for (let o = 0; o < respuestas.value; o++) {
 			let ul = document.createElement("ul");
 			ul.classList.add("main__container__list");
@@ -54,6 +67,7 @@ function creador(event) {
 			input.name = nameInput;
 			const containers = document.querySelectorAll(".main__container__trivia");
 			label.innerText = prompt("Inserta las posibles respuestas");
+
 			for (let cadaPregunta of containers) {
 				cadaPregunta.appendChild(ul);
 				ul.appendChild(li);
@@ -63,6 +77,6 @@ function creador(event) {
 		}
 	}
 
-	selecctionInput();
+	selectionInput();
 	reset();
 }
