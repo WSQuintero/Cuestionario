5680ba72a311af65ad32301cb107ad5745d8302b
Santiago <santiagoquinteroperfil@gmail.com>
2023-03-21 20:24:30 -0400

    agregando campso para escribir preguntas y respuestas directamente

--- javascript/creador.js
+++ javascript/creador.js
@@ -7,11 +7,15 @@ const form = document.querySelector(".form");
 buttonCreator.addEventListener("click", creador);
 
 function selectionInput() {
+    
 	const li = document.querySelectorAll("li");
 	li.forEach((a) => {
+        
 		a.querySelectorAll("input").forEach((b) => {
 			a.addEventListener("click", () => {
-				b.checked = true;
+                
+				if(!b.checked)b.checked = true;
+                else b.checked = false;
 			});
 		});
 	});
@@ -35,23 +39,30 @@ function creador(event) {
 		let containerQuestions = document.createElement("div");
 		containerQuestions.classList.add("main__container__trivia");
 		container.appendChild(containerQuestions);
-		const h2 = document.createElement("h2");
+        const containerPregunta = document.createElement("div");
+        containerPregunta.classList.add("containerPregunta")
+       
+        containerQuestions.appendChild(containerPregunta);
         const inputPregunta=document.createElement("input");
+        containerPregunta.appendChild(inputPregunta) ;
         inputPregunta.type="text";
-        containerQuestions.appendChild(inputPregunta) ;
 		const nameInput = ( inputPregunta.value);
-		h2.classList.add("main__list__tittle");
-		containerQuestions.appendChild(h2);
+		
         const buttonPregunta=document.createElement("button");
-        buttonPregunta.innerText="Crear";
+        buttonPregunta.innerText="Crear pregunta";
         buttonPregunta.classList.add("buttonPregunta");
-        containerQuestions.appendChild(buttonPregunta);
-
+        containerPregunta.appendChild(buttonPregunta);
+        inputPregunta.placeholder="Escibe la pregunta aquí"
+            const h2 = document.createElement("h2");
+            h2.classList.add("main__list__tittle");
+            containerPregunta.appendChild(h2);
+            h2.classList.add("remover");
+            
         buttonPregunta.addEventListener("click",()=>{
+            h2.classList.remove("remover");
             h2.innerText=inputPregunta.value;
             buttonPregunta.classList.add("remover")
             inputPregunta.classList.add("remover")
-            console.log("hola")
         })
 		for (let o = 0; o < respuestas.value; o++) {
 			let ul = document.createElement("ul");
@@ -65,8 +76,26 @@ function creador(event) {
 			input.classList.add("main__input__radio");
 			input.type = "radio";
 			input.name = nameInput;
+            input.classList.add("remover");
 			const containers = document.querySelectorAll(".main__container__trivia");
-			label.innerText = prompt("Inserta las posibles respuestas");
+            const inputRespuestas=document.createElement("input");
+            inputRespuestas.classList.add("inputRespuestas");
+            inputRespuestas.placeholder="Escribe la opción de respuesta aquí";
+            inputRespuestas.classList.add("inputRespuestas")
+            label.appendChild(inputRespuestas);
+            const ButtonCrearRespuesta=document.createElement("button");
+            ButtonCrearRespuesta.innerText="Crear";
+            label.appendChild(ButtonCrearRespuesta);
+            ButtonCrearRespuesta.addEventListener("click",()=>{
+                label.innerText = inputRespuestas.value;
+                ButtonCrearRespuesta.classList.add("remover")
+                inputRespuestas.classList.add("remover");
+                input.classList.remove("remover");
+                
+                selectionInput();
+                
+            })
+			
 
 			for (let cadaPregunta of containers) {
 				cadaPregunta.appendChild(ul);
@@ -76,7 +105,7 @@ function creador(event) {
 			}
 		}
 	}
-
-	selectionInput();
+    
+	
 	reset();
 }
