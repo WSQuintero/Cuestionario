4d8f190b0ad0e1ee0aeb9104b90be1be919373b4
Santiago <santiagoquinteroperfil@gmail.com>
2023-03-22 20:52:04 -0400

    refactorizando código para hacerlo más legible y corrigiendo inputs

--- javascript/creador.js
+++ javascript/creador.js
@@ -7,15 +7,11 @@ const form = document.querySelector(".form");
 buttonCreator.addEventListener("click", creador);
 
 function selectionInput() {
-    
 	const li = document.querySelectorAll("li");
 	li.forEach((a) => {
-        
 		a.querySelectorAll("input").forEach((b) => {
 			a.addEventListener("click", () => {
-                
-				if(!b.checked)b.checked = true;
-                else b.checked = false;
+				b.checked = true;
 			});
 		});
 	});
@@ -35,77 +31,83 @@ function reset() {
 function creador(event) {
 	event.preventDefault();
 	divCreador.classList.add("remover");
+
 	for (let i = 0; i < preguntas.value; i++) {
-		let containerQuestions = document.createElement("div");
+		const containerQuestions = document.createElement("div");
+		const containerPregunta = document.createElement("div");
+		const inputPregunta = document.createElement("input");
+		const buttonPregunta = document.createElement("button");
+		const h2 = document.createElement("h2");
+		const ul = document.createElement("ul");
+		const generalLabel = document.createElement("label");
+
 		containerQuestions.classList.add("main__container__trivia");
+		containerPregunta.classList.add("containerPregunta");
+		buttonPregunta.classList.add("buttonPregunta");
+		h2.classList.add("main__list__tittle");
+		h2.classList.add("remover");
+
+		inputPregunta.type = "text";
+		buttonPregunta.innerText = "Crear pregunta";
+		inputPregunta.placeholder = "Escibe la pregunta aquí";
+
 		container.appendChild(containerQuestions);
-        const containerPregunta = document.createElement("div");
-        containerPregunta.classList.add("containerPregunta")
-       
-        containerQuestions.appendChild(containerPregunta);
-        const inputPregunta=document.createElement("input");
-        containerPregunta.appendChild(inputPregunta) ;
-        inputPregunta.type="text";
-		const nameInput = ( inputPregunta.value);
-		
-        const buttonPregunta=document.createElement("button");
-        buttonPregunta.innerText="Crear pregunta";
-        buttonPregunta.classList.add("buttonPregunta");
-        containerPregunta.appendChild(buttonPregunta);
-        inputPregunta.placeholder="Escibe la pregunta aquí"
-            const h2 = document.createElement("h2");
-            h2.classList.add("main__list__tittle");
-            containerPregunta.appendChild(h2);
-            h2.classList.add("remover");
-            
-        buttonPregunta.addEventListener("click",()=>{
-            h2.classList.remove("remover");
-            h2.innerText=inputPregunta.value;
-            buttonPregunta.classList.add("remover")
-            inputPregunta.classList.add("remover")
-        })
+		containerQuestions.appendChild(containerPregunta);
+		containerPregunta.appendChild(inputPregunta);
+		containerPregunta.appendChild(buttonPregunta);
+		containerPregunta.appendChild(h2);
+		generalLabel.appendChild(ul);
+
+		buttonPregunta.addEventListener("click", () => {
+			h2.classList.remove("remover");
+			h2.innerText = inputPregunta.value;
+			buttonPregunta.classList.add("remover");
+			inputPregunta.classList.add("remover");
+		});
+
 		for (let o = 0; o < respuestas.value; o++) {
-			let ul = document.createElement("ul");
-			ul.classList.add("main__container__list");
-			let li = document.createElement("li");
-			li.classList.add("main__element_list");
-			let label = document.createElement("label");
-			label.classList.add("main__checkbox__label");
-			label.for = nameInput;
-			let input = document.createElement("input");
-			input.classList.add("main__input__radio");
-			input.type = "radio";
-			input.name = nameInput;
-            input.classList.add("remover");
+			const li = document.createElement("li");
+			const label = document.createElement("label");
+			const input = document.createElement("input");
+			const inputRespuestas = document.createElement("input");
+			const ButtonCrearRespuesta = document.createElement("button");
 			const containers = document.querySelectorAll(".main__container__trivia");
-            const inputRespuestas=document.createElement("input");
-            inputRespuestas.classList.add("inputRespuestas");
-            inputRespuestas.placeholder="Escribe la opción de respuesta aquí";
-            inputRespuestas.classList.add("inputRespuestas")
-            label.appendChild(inputRespuestas);
-            const ButtonCrearRespuesta=document.createElement("button");
-            ButtonCrearRespuesta.innerText="Crear";
-            label.appendChild(ButtonCrearRespuesta);
-            ButtonCrearRespuesta.addEventListener("click",()=>{
-                label.innerText = inputRespuestas.value;
-                ButtonCrearRespuesta.classList.add("remover")
-                inputRespuestas.classList.add("remover");
-                input.classList.remove("remover");
-                
-                selectionInput();
-                
-            })
-			
+
+			input.type = "radio";
+			input.name = `Pregunta${i}`;
+			generalLabel.htmlFor = `Pregunta${i}`;
+
+			ul.classList.add("main__container__list");
+			li.classList.add("main__element_list");
+			label.classList.add("main__checkbox__label");
+			input.classList.add("main__input__radio");
+			input.classList.add("remover");
+			inputRespuestas.classList.add("inputRespuestas");
+			inputRespuestas.classList.add("inputRespuestas");
+
+			inputRespuestas.placeholder = "Escribe la opción de respuesta aquí";
+			ButtonCrearRespuesta.innerText = "Crear";
+
+			label.appendChild(inputRespuestas);
+			label.appendChild(ButtonCrearRespuesta);
+
+			ButtonCrearRespuesta.addEventListener("click", () => {
+				label.innerText = inputRespuestas.value;
+				ButtonCrearRespuesta.classList.add("remover");
+				inputRespuestas.classList.add("remover");
+				input.classList.remove("remover");
+				selectionInput();
+				
+			});
 
 			for (let cadaPregunta of containers) {
-				cadaPregunta.appendChild(ul);
+				cadaPregunta.appendChild(generalLabel);
 				ul.appendChild(li);
-				li.appendChild(input);
 				li.appendChild(label);
+				li.appendChild(input);
 			}
 		}
 	}
-    
 	
 	reset();
 }
