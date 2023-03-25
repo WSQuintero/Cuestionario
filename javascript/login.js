137df6df98f60071789760548d44c11e3077815a
Santiago <santiagoquinteroperfil@gmail.com>
2023-03-24 20:24:36 -0400

    corrigiendo login

--- javascript/login.js
+++ javascript/login.js
@@ -1,39 +1,34 @@
-let baseUsuarios = JSON.parse(localStorage.getItem("baseUsuarios")) || [];
-const usuario = document.querySelector("#usuario");
-const password = document.querySelector("#password");
-const copia = [...baseUsuarios];
+const baseUsuarios = JSON.parse(localStorage.getItem('baseUsuarios')) || []
+const usuario = document.querySelector('#usuario')
+const password = document.querySelector('#password')
+const copia = [...baseUsuarios]
 
+const button = document.getElementById('button')
+const error = document.querySelector('#error')
 
-const linkRegistrar = document.getElementById("registrar");
-const button = document.getElementById("button");
-const error = document.querySelector("#error");
+button.addEventListener('click', ingresar)
 
-button.addEventListener("click", ingresar);
-
-
-
-
-function ingresar(event) {
-	const busqueda = copia.find(
-		(a) => a.user === usuario.value && a.pass === password.value
-	);
-	localStorage.setItem("usuarioActual", JSON.stringify(busqueda));
-	if (busqueda === undefined) {
-		error.innerText = "El usuario o contraseña son incorrectos";
-		error.style.color = "red";
-		error.style.textAlign = "center";
-		event.preventDefault();
-	} else {
-		if (busqueda.tipo == "Estudiante") {
-			const link = document.createElement("a");
-			link.href = "./html/juego.html";
-			link.click();
-			event.preventDefault();
-		} else if (busqueda.tipo == "Profesor") {
-			const link = document.createElement("a");
-			link.href = "./html/creador.html";
-			link.click();
-			event.preventDefault();
-		}
-	}
+function ingresar (event) {
+  const busqueda = copia.find(
+    (a) => a.user === usuario.value && a.pass === password.value
+  )
+  localStorage.setItem('usuarioActual', JSON.stringify(busqueda))
+  if (busqueda === undefined) {
+    error.innerText = 'El usuario o contraseña son incorrectos'
+    error.style.color = 'red'
+    error.style.textAlign = 'center'
+    event.preventDefault()
+  } else {
+    if (busqueda.tipo === 'Estudiante') {
+      const link = document.createElement('a')
+      link.href = './html/juego.html'
+      link.click()
+      event.preventDefault()
+    } else if (busqueda.tipo === 'Profesor') {
+      const link = document.createElement('a')
+      link.href = './html/creador.html'
+      link.click()
+      event.preventDefault()
+    }
+  }
 }
